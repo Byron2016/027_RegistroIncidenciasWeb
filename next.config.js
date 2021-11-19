861a0880433f24dbc8c02f4118c11bfbfcfc2990
Byron 2016 <bgva2005@yahoo.com>
2021-11-19 14:12:28 -0500

    001_web V13 Agregar paquete next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-}
+};
+
+const withTranspileModules = require("next-transpile-modules")([
+  "@super2016/components_a",
+]);
+
+module.exports = withTranspileModules({});
